791dbd61808fb8acfeaa5c4c6ec2f7c0fb4fe67e
Nbdyleto <nobodyleto2004@gmail.com>
2022-08-10 15:58:02 -0300

    adding QTableWidget and show all tasks

--- daily-task/main.py
+++ daily-task/main.py
@@ -19,12 +19,26 @@ class Window(QWidget):
         global widgets
         widgets = self.ui
 
+        widgets.tableWidget.setColumnWidth(0,100)
+        widgets.tableWidget.setColumnWidth(1,100)
+        widgets.tableWidget.setColumnWidth(2,100)
+        widgets.tableWidget.setColumnWidth(3,150)
+
         self.main = Main()
 
+        """
         self.main.create_list('Math')
-        self.main.create_task(0, str(widgets.calendarWidget.selectedDate().toPython()), '2', 'Do some exercises')
-        self.main.create_task(0, str(widgets.calendarWidget.selectedDate().toPython()), '2', 'Do some exercises')
-        self.main.create_task(0, str(widgets.calendarWidget.selectedDate().toPython()), '2', 'Do some exercises')
+        self.main.create_task(0, str(QDate(2022, 8, 10).toPython()), '0', 'Do some exercises')
+        self.main.create_task(0, str(QDate(2022, 8, 11).toPython()), '1', 'Study Functions')
+        self.main.create_task(0, str(QDate(2022, 8, 11).toPython()), '2', 'Solve problems')
+        self.main.create_list('Geo')
+        self.main.create_task(1, str(widgets.calendarWidget.selectedDate().toPython()), '3', 'Study Rain')
+
+        self.main.create_task(0, str(QDate(2022, 8, 10).toPython()), '1', 'Study Math')
+        self.main.create_task(0, str(QDate(2022, 8, 12).toPython()), '2', 'Solve Calcule III')
+
+        self.main.create_task(1, str(widgets.calendarWidget.selectedDate().toPython()), '3', 'Study Geopolitcs')
+        """
 
         self.main.update_json()
 
@@ -41,6 +55,8 @@ class Window(QWidget):
         print(f'date: {date_selected}')
         self.updateTableWidget(date_selected)
 
+    # PySide6.QtCore.QDate(2022, 8, 10)
+
     def updateTableWidget(self, date):
         widgets.tableWidget.clear()
 
@@ -52,18 +68,18 @@ class Window(QWidget):
             first_key = next(iter(l))   # Get the first key for a list
             first_keys.append(first_key)
 
+        row = 0
         for index, result in enumerate(results['lists']):
             items = result[first_keys[index]] # items receive all items from specific list_key.
-            widgets.tableWidget.setRowCount(len(items))
+            widgets.tableWidget.setRowCount(15) # [ ] Set a valid row count...
             for it in items:
-                if (it['start_time'] == str(date)):
-                    print(it['active_list'], it['start_time'], it['end_time'], it['name'], '\n')
+                print(row, it['active_list'], it['start_time'], it['end_time'], it['name'], '\n')
                     
-                    # why is not displaying? :
-                    widgets.tableWidget.setItem(index, 0, QTableWidgetItem(it['active_list']))
-                    widgets.tableWidget.setItem(index, 1, QTableWidgetItem(it['start_time']))
-                    widgets.tableWidget.setItem(index, 2, QTableWidgetItem(it['end_time']))
-                    widgets.tableWidget.setItem(index, 3, QTableWidgetItem(it['name']))
+                widgets.tableWidget.setItem(row, 0, QTableWidgetItem(it['active_list']))
+                widgets.tableWidget.setItem(row, 1, QTableWidgetItem(it['start_time']))
+                widgets.tableWidget.setItem(row, 2, QTableWidgetItem(it['end_time']))
+                widgets.tableWidget.setItem(row, 3, QTableWidgetItem(it['name']))
+                row += 1
                     
     def updateTaskList(self, date):
         widgets.tasksListWidget.clear()
--- daily-task/ui_main.py
+++ daily-task/ui_main.py
@@ -29,16 +29,18 @@ class Ui_Form(object):
         self.calendarWidget.setGeometry(QRect(40, 150, 411, 311))
 
         self.tableWidget = QTableWidget(Form)
-        if (self.tableWidget.columnCount() < 3):
-            self.tableWidget.setColumnCount(3)
+        if (self.tableWidget.columnCount() < 4):
+            self.tableWidget.setColumnCount(4)
         __qtablewidgetitem = QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
         __qtablewidgetitem1 = QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
         __qtablewidgetitem2 = QTableWidgetItem()
         self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
         self.tableWidget.setObjectName(u"tableWidget")
-        self.tableWidget.setGeometry(QRect(480, 150, 341, 301))
+        self.tableWidget.setGeometry(QRect(480, 150, 480, 301))
 
         self.label = QLabel(Form)
         self.label.setObjectName(u"label")
@@ -56,8 +58,14 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
-        #self.saveButton.setText(QCoreApplication.translate("Form", u"Save Changes", None))
-        #self.addButton.setText(QCoreApplication.translate("Form", u"Add new", None))
         self.label.setText(QCoreApplication.translate("Form", u"Daily Task Planner", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Form", u"Name", None))
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Form", u"Age", None))
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("Form", u"Address", None))
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("Form", u"Test", None))
     # retranslateUi
 
